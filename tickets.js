800be6d7f263842db3370ca315c251ab067504f6
fernandocordeschi <143556012+fernandocordeschi@users.noreply.github.com>
2026-03-18 18:44:26 -0300

    Implement close and delete ticket functionalities
    Added functions to close and delete tickets with appropriate logging.

--- tickets.js
+++ tickets.js
@@ -29,7 +29,46 @@ function listTickets(){
 
 }
 
+function closeTicket(id){
+
+    let ticket = tickets.find(ticket => ticket.id === id);
+
+    if(ticket){
+        ticket.status = "Closed";
+        console.log("Ticket closed:", ticket);
+    }
+    else{
+        console.log("Ticket not found");
+    }
+
+}
+
+function deleteTicket(id){
+
+    let initialLength = tickets.length;
+
+    tickets = tickets.filter(ticket => ticket.id !== id);
+
+    if(tickets.length < initialLength){
+        console.log("Ticket deleted");
+    }
+    else{
+        console.log("Ticket not found");
+    }
+
+}
+
+/* TESTING AREA */
+
 createTicket("Fernando","Cannot login","High");
 createTicket("Martin","Printer not working","Low");
+
+listTickets();
+
+closeTicket(1);
+
+listTickets();
+
+deleteTicket(2);
 
 listTickets();
